3845656bc8e0c9ef3a97bf0b68fe12e94d40360b
Ofir Cohen <ofirch@mta.ac.il>
2019-05-23 18:50:53 +0300

    push

--- Ex03.GarageLogic/Wheel.cs
+++ Ex03.GarageLogic/Wheel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Ex03.Exceptions;
 
 namespace Ex03.GarageLogic
 {
@@ -11,26 +10,6 @@ namespace Ex03.GarageLogic
         string m_Manufacturer;
         float m_CurrentAirPressure;
         float m_MaxAirPressure;
-
-        public Wheel(string i_Manufacturer, 
-                     float i_CurrentAirPressure,
-                     float i_MaxAirPressure)
-        {
-            m_Manufacturer = i_Manufacturer;
-            m_CurrentAirPressure = i_CurrentAirPressure;
-            m_MaxAirPressure = i_MaxAirPressure;
-        }
-
-        public float MaxAirPressure
-        {
-            get { return m_MaxAirPressure; }
-        }
-
-        public float CurrentAirPressure
-        {
-            get { return m_CurrentAirPressure; }
-            set { m_CurrentAirPressure = value; }
-        }
 
         public void Inflate(float i_AirToAdd)
         {
