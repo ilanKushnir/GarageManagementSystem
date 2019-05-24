145c0d9cd92990c7917fa9e78169b5d32f3e24b5
Ilan Kushnir <ilanku@mta.ac.il>
2019-05-24 15:41:24 +0300

    Added first exceptions to UI class

--- Ex03.ConsoleUI/UI.cs
+++ Ex03.ConsoleUI/UI.cs
@@ -10,22 +10,37 @@ namespace Ex03.ConsoleUI
     {
         public static void Run(Garage i_garage)
         {
-            string userInput = string.Empty;
-            int userChoice = 0;
-            bool valueInRange = false;
-            Console.WriteLine("Welcome To Garage Manager By Ilan & Ofir" + Environment.NewLine);
+            //string userInput = string.Empty;
+            int    userChoice = 0;
+            bool   validInput = false;
 
-            while(userChoice != 7)
+            PrintWelcomeMessage();
+
+            while (userChoice != 7)
             {
-                ShowMenu();
+                PrintMenu();
 
-                while(valueInRange == false)
+                while(validInput == false)
                 {
-                    userChoice = getUserChoice();
-                    valueInRange = validateUserChoice(userChoice);
+                    try
+                    {
+                        validInput = GetValidUserIntChoiceInRange(out userChoice, 1, 7);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error: Please enter a number.");
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Error: The number should be between 1 to 7.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unknown error occured: " + Environment.NewLine + ex.Message + Environment.NewLine);
+                    }
                 }
 
-                switch(userChoice)
+                switch (userChoice)
                 {
                     case 1:
                         AddNewVehicleToGarage();
@@ -51,68 +66,87 @@ namespace Ex03.ConsoleUI
             // exit program
         }
 
-        public static int getUserChoice()
+        public static bool GetValidUserIntChoiceInRange(out int o_UserChoice, int i_MinValue, int i_MaxValue)
         {
-            try
+            if (!int.TryParse(Console.ReadLine(), out o_UserChoice))
             {
-                return Int32.Parse(Console.ReadLine());
+                throw new FormatException();
             }
-            catch (FormatException ex)
+
+            if (o_UserChoice < i_MinValue || o_UserChoice > i_MaxValue)
             {
-                Console.WriteLine(ex.Message);
-                return 0;
+                throw new ArgumentOutOfRangeException();
             }
+
+            return true;
         }
 
-        public static bool validateUserChoice(int userChoice)
+
+        public static void PrintWelcomeMessage()
         {
-            return userChoice <= 7 && userChoice >= 1;
+            StringBuilder welcome = new StringBuilder();
+            // ASCII art
+            welcome.Append(Environment.NewLine + " Welcome to:                    /\\\\      _____                    \r\n                 ,-----,       /  \\\\____/__|__\\_                  \r\n              ,--'---:---`--, /  |  _     |     `|                \r\n             ==(o)-----(o)==J    `(o)-------(o)=                  \r\n``````````````````````````````````````````````````````````````````\r\n  _____                         __  ___                           \r\n / ___/__  _______  ___  ___   /  |/  /__  ___  ___  ___  ___ ____\r\n/ (_ / _ `/ __/ _ `/ _ `/ -_) / /|_/ / _ `/ _ \\/ _ `/ _ `/ -_) __/\r\n\\___/\\_,_/_/  \\_,_/\\_, /\\__/ /_/  /_/\\_,_/_//_/\\_,_/\\_, /\\__/_/   \r\n                  /___/                            /___/          \r\n                          By Ilan & Ofir                          \r\n");
+            Console.WriteLine(welcome);
         }
 
-        public static void ShowMenu()
+        public static void PrintMenu()
         {
-            StringBuilder menu = new StringBuilder("Menu" + Environment.NewLine + "Please choose which action to make by inserting a chioce number below:" + Environment.NewLine);
-            menu.Append("1) Add a new vehicle to garage" + Environment.NewLine);
-            menu.Append("2) Show license numbers by status" + Environment.NewLine);
-            menu.Append("3) Change car status" + Environment.NewLine);
-            menu.Append("4) Inflate car wheels to maximum" + Environment.NewLine);
-            menu.Append("5) Fuel/Charge vehicle" + Environment.NewLine);
-            menu.Append("6) Show vehicle full data" + Environment.NewLine);
-            menu.Append("7) Exit" + Environment.NewLine);
-            menu.Append("Please choose: " + Environment.NewLine);
+            StringBuilder menu = new StringBuilder();
+            menu.Append(" Please choose which action to make by inserting a chioce number below: " + Environment.NewLine);
+            menu.Append(" __________________________________" + Environment.NewLine);
+            menu.Append("  1) Add a new vehicle to garage" +    Environment.NewLine);
+            menu.Append("  2) Show license numbers by status" + Environment.NewLine);
+            menu.Append("  3) Change car status" +              Environment.NewLine);
+            menu.Append("  4) Inflate car wheels to maximum" +  Environment.NewLine);
+            menu.Append("  5) Fuel/Charge vehicle" +            Environment.NewLine);
+            menu.Append("  6) Show vehicle full data" +         Environment.NewLine);
+            menu.Append("  7) Exit" +                           Environment.NewLine);
+            menu.Append(" ----------------------------------" + Environment.NewLine + Environment.NewLine);
+            menu.Append(" Please choose and press Enter: " +    Environment.NewLine);
+
             Console.WriteLine(menu);
         }
 
-        
+
         ////////////////////////////////
-        
+
 
         public static void AddNewVehicleToGarage()
         {
             int userChoice = 0;
-            StringBuilder userPrompt = new StringBuilder("Please choose vehicle type:" + Environment.NewLine);
-            userPrompt.Append(" 1) Car" + Environment.NewLine);
-            userPrompt.Append(" 2) Electric Car" + Environment.NewLine);
-            userPrompt.Append(" 3) Motorcycle" + Environment.NewLine);
-            userPrompt.Append(" 4) Electric Motorcycle" + Environment.NewLine);
-            userPrompt.Append(" 5) Truck" + Environment.NewLine);
+            bool validInput = false;
+            StringBuilder userPrompt = new StringBuilder();
+            userPrompt.Append(" ---------------------------------" + Environment.NewLine);
+            userPrompt.Append(" Please choose your vehicle type:" +  Environment.NewLine);
+            userPrompt.Append("  1) Car" +                           Environment.NewLine);
+            userPrompt.Append("  2) Electric Car" +                  Environment.NewLine);
+            userPrompt.Append("  3) Motorcycle" +                    Environment.NewLine);
+            userPrompt.Append("  4) Electric Motorcycle" +           Environment.NewLine);
+            userPrompt.Append("  5) Truck" +                         Environment.NewLine);
+            userPrompt.Append(" ---------------------------------" + Environment.NewLine);
 
             Console.Write(userPrompt + Environment.NewLine);
 
-            while(userChoice < 1 && userChoice > 5)
+            while(validInput == false)
             {
-                userChoice = Int32.Parse(Console.ReadLine());
-
-                //catch(FormatException ex)
+                try
                 {
-
+                    validInput = GetValidUserIntChoiceInRange(out userChoice, 1, 5);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Please enter a number.");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Error: The number should be between 1 to 5.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unknown error occured: " + Environment.NewLine + ex.Message + Environment.NewLine);
                 }
             }
-
-
-
-
-
         }
 
         public static void ShowLicenseNumbersByStatus()
